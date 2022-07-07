f6b7baeda86e28fd25364572aa17940f6c630143
nilamnagwade <pacharnenn244@gmail.com>
2022-07-07 22:22:57 +0530

    first program

---
+++ substraction.java
@@ -0,0 +1,30 @@
+ class substraction{
+             int number1;
+             int number2; 
+
+      void dosubstraction(){
+
+      int sub = number1 - number2;
+
+      system.out.println(sub);  
+
+     }
+
+ }     
+  class JVM Entry point {  
+        
+      public static void main(sring[]arg);{
+
+          substrction s1 =new substraction();
+           
+           s1.number1 = 40;
+           s1.number2 = 20; 
+           s1.do substraction();
+            }
+
+       }
+
+
+
+
+}     
